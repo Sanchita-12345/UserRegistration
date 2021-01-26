46e40f3b40d1827e203050c6c9d83b081ce62b7c
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-26 15:52:44 -0800

    User Registration Program phone number added

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,8 +4,8 @@ class UserRegistration{
 
 	public static void main(String args[])
 	{
-		Pattern pat = Pattern.compile("^[a-z0-9.]+@[a-z.]+$");
-		Matcher match = pat.matcher("abc.xyz@bl.co.in");
+		Pattern pat = Pattern.compile("^[1-9]{2}\\s[0-9]{10}$");
+		Matcher match = pat.matcher("91 9919819801");
 		boolean result = match.matches();
 		System.out.println(result);
 
