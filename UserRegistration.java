7fc06ae2608ea7a4081eb8b7d82daafd7ad2e243
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-25 22:46:30 -0800

    UserRegistration program Last name added part done

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,8 +4,8 @@ class UserRegistration{
 
 	public static void main(String args[])
 	{
-		Pattern pat = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-		Matcher match = pat.matcher("Sanchita");
+		Pattern pat = Pattern.compile("(^[A-Z]{1}[a-z]{2,})+\\s+([A-Z]{1}[a-z]{2,})*$");
+		Matcher match = pat.matcher("Sanchita Barik");
 		boolean result = match.matches();
 		System.out.println(result);
 
