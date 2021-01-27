46314ce21e0d0bbc61b854d8a4e96a9549b7aa8a
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-26 18:11:32 -0800

    User Registration Program password rule 4 part done

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,8 +4,8 @@ class UserRegistration{
 
 	public static void main(String args[])
 	{
-		Pattern pat = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$");
-		Matcher match = pat.matcher("Sanchita12345");
+		Pattern pat = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8,}$");
+		Matcher match = pat.matcher("Sanchita#98765");
 		boolean result = match.matches();
 		System.out.println(result);
 
