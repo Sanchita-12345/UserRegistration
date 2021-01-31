045118cf284053003808499aaf8131adb55bac35
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-01-31 17:29:13 +0530

    User Registration JUnit Testing part done

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,14 +1,46 @@
 import java.util.regex.*;
+import java.util.ArrayList;
 
 class UserRegistration{
 
 	public static void main(String args[])
 	{
-		Pattern pat = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8,}$");
-		Matcher match = pat.matcher("Sanchita#98765");
-		boolean result = match.matches();
-		System.out.println(result);
+		ArrayList<String> emails = new ArrayList<String>();
 
+		//Valid Emails
+		emails.add("abc@yahoo.com,");
+		emails.add("abc-100@yahoo.com,");
+		emails.add("abc.100@yahoo.com");
+	   emails.add("abc111@abc.com,");
+      emails.add("abc-100@abc.net,");
+      emails.add("abc-100@abc.com.au");
+      emails.add("abc@1.com,");
+      emails.add("abc@gmail.com.com");
+      emails.add("abc+100@gmail.com");
+
+		//Invalid Emails
+		emails.add("abc");
+      emails.add("abc@.com.my");
+      emails.add("abc123@gmail.a");
+      emails.add("abc123@.com");
+      emails.add("abc123@.com.com");
+      emails.add(".abc@abc.com");
+      emails.add("abc()*@gmail.com");
+      emails.add("abc@%*.com");
+      emails.add("abc..2002@gmail.com");
+      emails.add("abc@gmail.com");
+      emails.add("abc@abc@gmail.com");
+      emails.add("abc@gmail.com.1a");
+      emails.add("abc@gmail.com.aa.au");
+
+		String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,10}$";
+		Pattern pat = Pattern.compile(regex);
+		for (String email : emails)
+		{
+			Matcher match = pat.matcher(email);
+			boolean result = match.matches();
+			System.out.println(email +" : "+ result);
+		}
 	}
-
 }
+
---
+++ UserRegistrationMain.java
@@ -0,0 +1,56 @@
+package com.bridgelabz;
+
+import java.util.regex.Pattern;
+
+public class UserRegistrationMain {
+
+   	private static final String firstName = "^[A-Z]{1}[a-z]{2,}$";
+   	private static final String lastName = "(^[A-Z]{1}[a-z]{2,})+\\s+([A-Z]{1}[a-z]{2,})*$";
+   	private static final String email = "^[a-z0-9.]+@[a-z.]+$";
+   	private static final String phoneNumber = "^[1-9]{2}\\s[0-9]{10}$";
+   	private static final String passwordOne = "^[a-z]{8,}$";
+   	private static final String passwordTwo = "^(?=.*[A-Z])(?=.*[a-z]).{8,}$";
+   	private static final String passwordThree = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$";
+   	private static final String passwordFour = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@#$%^&-+=()]).{8,}$";
+	
+	public boolean  getFirstName(String first_name) {
+		Pattern pattern = Pattern.compile(firstName);
+		return pattern.matcher(first_name).matches();
+	}
+
+	public boolean  getLastName(String last_name) {
+		Pattern pat = Pattern.compile(lastName);
+		return pattern.matcher(last_name).matches();
+	}
+	
+	
+	public boolean  getEmail(String emailId) {
+		Pattern pat = Pattern.compile(email);
+		return pattern.matcher(emailId).matches();
+	}
+	
+	public boolean  getMobileNumber(String ph_no) {
+		Pattern pat = Pattern.compile(phoneNumber);
+		return pattern.matcher(ph_no).matches();
+	}
+	
+	public boolean  getPasswordRuleOne(String password_one) {
+		Pattern pat = Pattern.compile(passwordOne);
+		return pattern.matcher(password_one).matches();
+	}
+	
+	public boolean  getPasswordRuleTwo(String password_two) {
+		Pattern pat = Pattern.compile(passwordTwo);
+		return pattern.matcher(password_two).matches();
+	}
+	
+	public boolean  getPasswordRuleThree(String password_three) {
+		Pattern pat = Pattern.compile(passwordThree);
+		return pattern.matcher(password_three).matches();
+	}
+	
+	public boolean  getPasswordRuleThree(String password_four) {
+		Pattern pat = Pattern.compile(passwordFour);
+		return pattern.matcher(password_four).matches();
+	}
+}
